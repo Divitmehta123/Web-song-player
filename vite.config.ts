d544487b1884cc01a90955c5117c58a2a7b09757
Divitmehta123 <142271478+Divitmehta123@users.noreply.github.com>
2025-10-31 00:50:06 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(() => ({
-  base: "/<REPO_NAME>/",
+  base: "/<Web-song-player>/",
   server: {
     host: "::",
     port: 8080,
